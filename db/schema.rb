630a26fb38bdab430c092c904cc752e358ca8e25
mariabaltabaeva <maria.baltabaeva@gmail.com>
2021-07-20 20:55:57 -0400

    DB changed

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_07_16_005805) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "friends", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
